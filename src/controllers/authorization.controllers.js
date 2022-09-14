2bf0a1af46e917a1f31c262a1edc0dbe392b8398
Nathalia <nathalia.gramst@gmail.com>
2022-09-14 16:41:02 -0300

    Controller for sign-up

--- src/controllers/authorization.controllers.js
+++ src/controllers/authorization.controllers.js
@@ -1,9 +1,19 @@
 import db from "../database/db.js";
+import bcrypt from "bcrypt";
 
 async function signUp(req, res) {
+    const { username, email, password } = req.body;
 
     try {
-        res.send('Ok');
+        const passwordHash = bcrypt.hashSync(password, 10);
+
+        await db.collection("users").insertOne({
+            username,
+            email,
+            password: passwordHash
+        });
+
+        res.status(201).send({ message: "User created successfully" });
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
