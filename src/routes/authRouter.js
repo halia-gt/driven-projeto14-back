f836c0e251b0aec2ac83445bd0459fd1f435680f
Nathalia <nathalia.gramst@gmail.com>
2022-09-15 15:39:16 -0300

    Renaming files to match pattern

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { authValidation } from "../middlewares/authorization.middleware.js";
-import * as authController from "../controllers/authorization.controllers.js";
+import { authValidation } from "../middlewares/authMiddleware.js";
+import * as authController from "../controllers/authController.js";
 
 const router = express.Router();
 
