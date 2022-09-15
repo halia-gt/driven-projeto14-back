9a6cf3092ac2507b4a8bcb66a77fecef96746b67
carlos_casanova <carlosctu94@gmail.com>
2022-09-15 19:13:49 -0300

    feat/Deploy/adjust code for deploy on heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,6 @@ app.use(express.json());
 app.use(cors());
 app.use(router);
 
-app.listen(5000, () => {
-  console.log("Magic happens on port 5000");
+app.listen(process.env, () => {
+  console.log("Magic happens on port " + process.env.PORT);
 });
