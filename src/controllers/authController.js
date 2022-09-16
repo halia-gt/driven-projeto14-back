0758b651e87442ad6af9946014e510f473adc239
carlos_casanova <carlosctu94@gmail.com>
2022-09-15 22:20:41 -0300

    testing api routes with heroku

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,31 +1,18 @@
-//import db from "../database/db.js";
+import db from "../database/db.js";
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
-import { MongoClient } from "mongodb";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const mongoClient = new MongoClient(process.env.MONGO_URI);
-let db;
 
 async function signUp(req, res) {
-    console.log("entrou")
-    const { username, email, password } = req.body;
-    try {
-      console.log("caiu")
-      await mongoClient.connect();
-      db = mongoClient.db("routtastore");
+  const { username, email, password } = req.body;
+  try {
     const passwordHash = bcrypt.hashSync(password, 10);
     await db.collection("users").insertOne({
-        username,
-        email,
-        password: passwordHash,
+      username,
+      email,
+      password: passwordHash,
     });
-    
     res.status(201).send({ message: "User created successfully" });
-} catch (error) {
-      console.log("não deu boa")
+  } catch (error) {
     console.log(error);
     res.sendStatus(500);
   }
